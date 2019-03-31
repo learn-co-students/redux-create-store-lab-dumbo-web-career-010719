85cb0c1788a66b8002171811495d557b382b7366
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-03-31 16:39:38 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,19 @@
 export default function createStore(reducer) {
+  let state=0;
+
+  const dispatch =(action)=>{
+    state=reducer(state, action)
+    render();
+  }
+
+  const getState = () =>{
+      return state;
+  }
+
+  return{
+    getState,
+    dispatch
+  }
   // add your code here
 }
 
