350e9b1c37d2492ab6e7849930d01c1d7ffd9f8b
Bennimaru <bennywhyu@gamil.com>
2019-04-04 16:16:13 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,25 @@
+// import countReducer from "./reducers/countReducer"
+// import candyReducer from "./reducers/candyReducer"
+
 export default function createStore(reducer) {
   // add your code here
+  let state
+
+  function dispatch(action){
+    state= reducer(state,action)
+    render()
+  }
+
+  function getState(){
+    return state
+  }
+
+  dispatch({type:"start"})
+
+  return {
+    dispatch,
+    getState
+  }
 }
 
 function render() {
